e90816a66f7cc191a5f07347af85701de57815e0
KazouDev <frenchkazou@gmail.com>
2025-10-10 11:21:21 +0200

    [feat] HoraireOuverture

---
+++ src/main/java/com/b2z/model/HoraireOuverture.java
@@ -0,0 +1,12 @@
+package com.b2z.model;
+
+import com.b2z.utils.JourSemaine;
+
+import java.sql.Time;
+
+public class HoraireOuverture extends Horaire {
+    public HoraireOuverture(JourSemaine jourSemaine, Time heureOuverture, Time heureFermeture) {
+       super(jourSemaine, heureOuverture, heureFermeture);
+    }
+}
+
